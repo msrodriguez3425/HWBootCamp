3ca58f6e8c15ef2f91ff2da825fe776584c80f21
msrodriguez3425 <msrodriguez3425@gmail.com>
2019-03-02 12:39:59 -0600

    Add files via upload

---
+++ HouseOfPies.py
@@ -0,0 +1,37 @@
+print("Welcome to the house of Pies! Here are our pies:")
+
+pies = ["Pecan","Apple Crisp","Bean","Banoffee","Black Bun","Blueberry","Buko","Burek","Tamale","Steak"]
+for i in range(3):
+    print(" ")
+
+print("-------------------------------------------------")
+
+Line = ""
+for i in range(10):
+    Line = Line + "(" + str(i+1) + ") " + pies[i] + ", "
+
+print (Line)
+
+pieCount = [0,0,0,0,0,0,0,0,0,0]
+more = True
+pieList = []
+while more == True:
+    selected = input("Which would you like (number)? ")
+    pieCount[int(selected) -1] += 1
+    print(f"Great! we'll have that {pies[int(selected)-1]} right out for you.")
+    x = input ("Woul you like to make another purchase? (y)es or (n)o")
+    if x == "y":
+        continue
+        #nothing
+    else:
+        more = False
+        print("Your purchases are the following: ")
+        for i in range(len(pieCount)):
+            if pieCount[i] == 0:
+                continue
+            else:
+                print(f"{pieCount[i]} {pies[i]}") 
+
+        print("Thankyou for your purchases. Goodbye!")
+
+
